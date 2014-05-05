c71401addcf3588d4ef9fa977e40cd24c0d1687f
Tino Adams <tinoadams@Daciens-MacBook-Air.local>
2014-05-06 03:03:46 +1000

    Scroll file

--- 2.3/async/scalatra-atmosphere-embedded/src/main/scala/org/scalatra/example/atmosphere/ScrollableFile.scala
+++ 2.3/async/scalatra-atmosphere-embedded/src/main/scala/org/scalatra/example/atmosphere/ScrollableFile.scala
@@ -10,20 +10,145 @@ import scala.concurrent.Future
 import scala.concurrent.Promise
 import java.nio.ByteBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
+import java.nio.CharBuffer
 
-class ScrollableFile(val filename: String, val charsetName: String = "UTF-8") {
-  val bufferSize: Int = 20;
+class ScrollableFile(val filename: String, val charsetName: String = "UTF-8", val preferedBufferSize: Int = 20) {
+  val lineSeperator = "\n"
   val charset = Charset.forName(charsetName)
+  val stringDecoder = charset.newDecoder()
+  stringDecoder.onMalformedInput(CodingErrorAction.REPLACE)
+  stringDecoder.onUnmappableCharacter(CodingErrorAction.REPLACE)
+
+  // size the buffer so we can try and always fit whole chars in it
+  // eg. UTF-16 = 2 bytes should have a buffer size of 2, 4, 6...
+  val charByte = (1 / stringDecoder.averageCharsPerByte).toInt
+  val bufferSize = preferedBufferSize + (preferedBufferSize % charByte)
+  val lineSeperatorByte = (charByte * lineSeperator.length())
+
   //  private val handle = new RandomAccessFile(filename, "r")
-  private val channel = AsynchronousFileChannel.open(Paths.get(filename));
+  private val channel = AsynchronousFileChannel.open(Paths.get(filename))
+  private val bytes = ByteBuffer.allocate(bufferSize)
+  private val chars = CharBuffer.allocate(bufferSize)
 
-  private var start, end: Long = 0
+  private var cursor = 0L
+  private var lineSeperatorFound = false
+  case class Line(content: Option[String], cursor: Long)
 
-  def up(): String = {
-    ""
+  def up(): Future[Option[String]] = {
+    //    println("up:" + cursor)
+    up(cursor).map { line =>
+      cursor = line.cursor
+      //      println("done up:" + cursor)
+      line.content
+    }
   }
 
-  implicit def nioHandler: (Future[Integer], CompletionHandler[Integer, java.lang.Object]) = {
+  private def up(start: Long): Future[Line] = {
+    val line = read(start - bufferSize)
+    line.flatMap {
+      case Line(Some(str), cursor) =>
+        val eol = str.reverse.indexOf(lineSeperator)
+        lineSeperatorFound = eol != -1
+        if (eol == -1)
+          // recursively search for either lineSeperator or BOF, whilst preserving what we've already found
+          up(cursor - bufferSize).map {
+            case Line(None, position) => Line(Some(str), position)
+            case Line(Some(prepend), position) => Line(Some(prepend + str), position)
+          }
+        else {
+          //          println("FOUND REVERSE:" + eol)
+          //          println("FOUND:" + (str.length - eol))
+          //          println(s"STR ($str)")
+          val substr = str.substring(str.length - eol)
+          // adjust our virtual file pointer backwards to where we found the lineSeperator (in bytes)
+          val adjustBy = (charByte * substr.length()) + lineSeperatorByte
+          Future(Line(Some(substr), cursor - adjustBy))
+        }
+
+      case _ => line
+    }
+  }
+
+  def down(): Future[Option[String]] = {
+    //    println("down:" + cursor)
+    down(cursor).map { line =>
+      cursor = line.cursor
+      //      println("done down:" + cursor)
+      line.content
+    }
+  }
+
+  private def down(start: Long): Future[Line] = {
+    val line = read(start + (if (lineSeperatorFound) lineSeperatorByte else 0))
+    line.flatMap {
+      case Line(Some(str), cursor) =>
+        val eol = str.indexOf(lineSeperator)
+        lineSeperatorFound = eol != -1
+        if (eol == -1)
+          // recursively search for either lineSeperator or EOF, whilst preserving what we've already found
+          down(cursor).map {
+            case Line(None, position) => Line(Some(str), position)
+            case Line(Some(append), position) => Line(Some(str + append), position)
+          }
+        else {
+          val substr = str.substring(0, eol)
+          // adjust our virtual file pointer backwards to where we found the lineSeperator (in bytes)
+          val adjustBy = (charByte * str.length()) - (charByte * substr.length())
+          Future(Line(Some(substr), cursor - adjustBy))
+        }
+
+      case _ => line
+    }
+  }
+
+  private def read(start: Long): Future[Line] = {
+    //    println("READ:" + start)
+    def doRead(start: Long) = {
+      //      println("DOREAD:" + start + " - " + (start + bytes.remaining()))
+      val (future, handler) = nioHandler
+      channel.read(bytes, start, null, handler)
+      future.map { read =>
+        if (read == -1) {
+          Line(None, start)
+        }
+        else {
+          bytes.flip() // prepare buffer for reading
+          chars.clear() // empty chars
+          // start new decoding sequence
+          stringDecoder.reset()
+          stringDecoder.decode(bytes, chars, true)
+          stringDecoder.flush(chars)
+          chars.flip() // prepare decoded chars for reading
+          // progress our virtual file position by as many bytes as the decoder has read
+          Line(Some(chars.toString), start + bytes.position())
+        }
+      }
+    }
+
+    // we assume that decodeBytes only returns the number of bytes that have been read to decode the string
+    // eg. 19 bytes read but only 18 used to decode UTF-16 string
+    bytes.clear
+
+    if (start < 0) {
+      // equivalent to EOF, if we are at least one buffer length away from the file start
+      if ((start + bufferSize) <= 0)
+        Future(Line(None, 0))
+      // read from start of file
+      else {
+        val limit = (start + bufferSize).toInt
+        //                println("LIMITING:" + limit)
+        bytes.limit(limit)
+        doRead(0)
+      }
+    }
+    else {
+      // clear potential left overs from last use
+      // normal read from given start position
+      doRead(start)
+    }
+  }
+
+  def nioHandler: (Future[Integer], CompletionHandler[Integer, java.lang.Object]) = {
     val promise = Promise[Integer]()
     val handler = new CompletionHandler[Integer, java.lang.Object]() {
       override def completed(result: Integer, attachment: java.lang.Object): Unit = promise.success(result)
@@ -31,44 +156,6 @@ class ScrollableFile(val filename: String, val charsetName: String = "UTF-8") {
     }
 
     (promise.future, handler)
-  }
-
-  def down(): Future[Option[String]] = {
-    start = end
-    down(start)
-  }
-
-  private def down(start: Long): Future[Option[String]] = {
-    read(start).flatMap {
-      case None =>
-        Future(None)
-      case Some(str) =>
-        println(str)
-        val eol = str.indexOf("""$""")
-        if (eol == -1)
-          down(end).map { _.map { append => str + append } }
-        else {
-          println(eol + "...")
-          val substr = str.substring(0, eol)
-          println(substr + "...")
-          end -= (str.getBytes().length - substr.getBytes().length) + 16
-          Future(Some(substr))
-        }
-    }
-  }
-
-  private def read(start: Long): Future[Option[String]] = {
-    val buffer = ByteBuffer.allocateDirect(bufferSize)
-    val (future, handler) = nioHandler
-    channel.read(buffer, start, null, handler)
-    future.map { read =>
-      end += read
-      if (read == -1) None
-      else {
-        buffer.rewind()
-        Some(charset.decode(buffer).toString)
-      }
-    }
   }
 
   def close = channel.close()
---
+++ 2.3/async/scalatra-atmosphere-embedded/src/test/scala/org/scalatra/example/atmosphere/ScrollableFileTest.scala
@@ -0,0 +1,69 @@
+package org.scalatra.example.atmosphere
+
+import org.junit.Assert._
+import org.junit.Test
+import scala.concurrent._
+import scala.concurrent.duration._
+
+class ScrollableFileTest {
+
+  @Test
+  def multipleLinesInBuffer() {
+    val filename = this.getClass().getResource("ShortTextUtf8.txt").getFile()
+    val f = new ScrollableFile(filename, "UTF-8", 20)
+    assertEquals(None, Await.result(f.up, 2 seconds))
+    assertEquals(Some("0"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("2"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("4"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("6"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("6"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("4"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("4"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("6"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("6"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("4"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("2"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("0"), Await.result(f.up, 2 seconds))
+    assertEquals(None, Await.result(f.up, 2 seconds))
+    assertEquals(None, Await.result(f.up, 2 seconds))
+    assertEquals(Some("0"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("2"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("4"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("6"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("8"), Await.result(f.down, 2 seconds))
+    assertEquals(None, Await.result(f.down, 2 seconds))
+    assertEquals(None, Await.result(f.down, 2 seconds))
+    assertEquals(Some("8"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("8"), Await.result(f.down, 2 seconds))
+  }
+
+  @Test
+  def fractionOfLinesInBuffer() {
+    val filename = this.getClass().getResource("LongTextUtf16.txt").getFile()
+    val f = new ScrollableFile(filename, "UTF-16", 20)
+    assertEquals(None, Await.result(f.up, 2 seconds))
+    assertEquals(Some("package org.scalatra.example.atmosphere"), Await.result(f.down, 2 seconds))
+    assertEquals(Some(""), Await.result(f.down, 2 seconds))
+    assertEquals(Some("; Default imports from a sto;ck Scalatra g8 code generator:"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("import org.;scalatra._"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("import org.;scalatra._"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("; Default imports from a sto;ck Scalatra g8 code generator:"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("; Default imports from a sto;ck Scalatra g8 code generator:"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("import org.;scalatra._"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("import org.;scalatra._"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("; Default imports from a sto;ck Scalatra g8 code generator:"), Await.result(f.up, 2 seconds))
+    assertEquals(Some(""), Await.result(f.up, 2 seconds))
+    assertEquals(Some("package org.scalatra.example.atmosphere"), Await.result(f.up, 2 seconds))
+    assertEquals(None, Await.result(f.up, 2 seconds))
+    assertEquals(None, Await.result(f.up, 2 seconds))
+    assertEquals(Some("package org.scalatra.example.atmosphere"), Await.result(f.down, 2 seconds))
+    assertEquals(Some(""), Await.result(f.down, 2 seconds))
+    assertEquals(Some("; Default imports from a sto;ck Scalatra g8 code generator:"), Await.result(f.down, 2 seconds))
+    assertEquals(Some("import org.;scalatra._"), Await.result(f.down, 2 seconds))
+    assertEquals(None, Await.result(f.down, 2 seconds))
+    assertEquals(None, Await.result(f.down, 2 seconds))
+    assertEquals(Some("import org.;scalatra._"), Await.result(f.up, 2 seconds))
+    assertEquals(Some("import org.;scalatra._"), Await.result(f.down, 2 seconds))
+  }
+
+}
